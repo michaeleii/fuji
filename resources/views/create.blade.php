6b3b0e33bb52c338939b45d83d99f67b724b9584
Michael Lei <michaelleie0ca@gmail.com>
2024-04-07 16:04:03 -0700

    Finish product create page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,2 +1,52 @@
-<x-layout heading="Create Recipe">
+<x-layout heading="Create Product">
+    <!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+    <form>
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-3">
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                        <div class="mt-2">
+                            <input type="text" name="name" id="name" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3">
+                        <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
+                        <div class="mt-2">
+                            <input type="number" name="price" id="last-name" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-4">
+                        <label for="image" class="block text-sm font-medium leading-6 text-gray-900">Image URL</label>
+                        <div class="mt-2">
+                            <input id="image" name="image" type="url" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add Product</button>
+        </div>
+    </form>
+
+
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,8 @@
         <h2 class="sr-only">Products</h2>
         <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             <x-product-item image="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" name="Earthen Bottle" price="48" />
-
-
-
             <x-product-item image="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" name="Nomad Tumbler" price="35" />
-
-
-
-
             <x-product-item image="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" name="Machined Mechanical Pencil" price="35" />
-
-
-
         </div>
     </div>
 </x-layout>
