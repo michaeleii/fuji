f2bbbfe1c3ad27d0cdbb92b3d20fd05bd85e1a65
Michael Lei <michaelleie0ca@gmail.com>
2024-04-07 15:24:23 -0700

    Style active link

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -5,7 +5,6 @@
            </div>
            <div class="hidden md:block">
                <div class="ml-10 flex items-baseline space-x-4">
-                   <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                    <x-nav-link href="/" :active="request()->is('/')">Dashboard</x-nav-link>
                    <x-nav-link href="/create" :active="request()->is('create')">Create</x-nav-link>
                    <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
--- resources/views/components/mobile-menu.blade.php
+++ resources/views/components/mobile-menu.blade.php
@@ -1,9 +1,9 @@
 <div class="md:hidden" id="mobile-menu">
     <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Create</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+        <x-nav-link href="/" mobile :active="request()->is('/')">Dashboard</x-nav-link>
+        <x-nav-link href="/create" mobile :active="request()->is('create')">Create</x-nav-link>
+        <x-nav-link href="/about" mobile :active="request()->is('about')">About</x-nav-link>
+
     </div>
     <div class="border-t border-gray-700 pb-3 pt-4">
         <div class="flex items-center px-5">
