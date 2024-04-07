a0812f09937d80261320711cb097191a21decee3
Michael Lei <michaelleie0ca@gmail.com>
2024-04-07 15:55:00 -0700

    Finish homepage

--- resources/views/components/product-item.blade.php
+++ resources/views/components/product-item.blade.php
@@ -1,6 +1,7 @@
-<a href="#" class="group border p-5 rounded-lg bg-white hover:bg-gray-100 transition-colors">
-    <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-    </div>
-    <h3 class="mt-4 text-sm text-gray-700">{{ $name }}</h3>
-    <p class="mt-1 text-lg font-medium text-gray-900">${{ $price }}</p>
-</a>
+ <a href="#" class="group">
+     <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+         <img src="{{ $image }}" alt="{{ $name }}" class="h-full w-full object-cover object-center group-hover:opacity-75">
+     </div>
+     <h3 class="mt-4 text-sm text-gray-700">{{ $name }}</h3>
+     <p class="mt-1 text-lg font-medium text-gray-900">${{ $price }}</p>
+ </a>
