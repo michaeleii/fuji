feedaefca3c58edce6e440616fc2f843bd3b33f6
Michael Lei <michaelleie0ca@gmail.com>
2024-04-11 09:00:26 -0700

    Edit spacing

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -11,12 +11,10 @@
                             <input value="{{ $product->name }}" type="text" name="name" id="name" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
-
                     <div class="sm:col-span-3">
                         <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
                         <div class="mt-2">
                             <input type="text" value="{{ $product->price }}" name="price" id="last-name" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-
                         </div>
                     </div>
                     <div class="sm:col-span-3">
@@ -39,7 +37,6 @@
             <button type="submit" class="rounded-md w-full sm:w-fit bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update Product</button>
         </div>
     </form>
-
     <form id="deleteProductForm" action="/products/{{ $product->id }}" method="POST">
         @csrf
         @method('DELETE')
