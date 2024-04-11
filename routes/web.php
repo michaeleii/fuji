a2c192654aa6519d9f0c762b55cd84ccd47a8592
Michael Lei <michaelleie0ca@gmail.com>
2024-04-10 20:04:16 -0700

    Create product migration

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view("/", "home");
+
+Route::view("/about", "about");
 
 Route::get("/create", function () {
     return view('create');
 });
 
-Route::get("/about", function () {
-    return view('about');
-});
+
